18b3f92148957c76cb0b4844a339820afdb9004c
Nicolas Gonzalez <ndgfrs@gmail.com>
2026-08-16 20:31:36 -0400

    feat(scripts): generate contents for SortKey.java

--- scripts/sort_key/main.py
+++ scripts/sort_key/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import pathlib
+import textwrap
 from typing import cast
 
 from constants import FAMILIES, VARIANTS
@@ -9,33 +10,56 @@ from family import Family
 from sort_key import SortKey
 from variant import Affix, Variant
 
+if __name__ == "__main__":
+    # Move longer identifiers to the front so, for example, `STONE_BRICK`
+    # matches before `STONE`, and `CHEST_BOAT` matches before `BOAT`.
+    FAMILIES.sort(key=lambda f: len(f.identifier), reverse=True)
+    VARIANTS.sort(key=lambda v: len(v.identifier), reverse=True)
+
+SORTKEY_JAVA = """
+package io.github.nicdgonzalez.autosort;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public record SortKey(
+        int category,
+        int family,
+        int variant) {{
+    /** Mapping for `Material` names to `SortKey`s. */
+    private static Map<String, SortKey> BY_NAME = new HashMap<>();
+
+    static {{
+        {0}
+    }}
+
+    public static Optional<SortKey> byName(String name) {{
+        return Optional.ofNullable(BY_NAME.get(name));
+    }}
+}}
+"""
+
+SORTKEY_JAVA_LINE = (
+    "BY_NAME.put({0}, new SortKey({1.category}, {1.family}, {1.variant});"
+)
+
 
 def main() -> None:
     args = build_cli().parse_args()
     # `parse_args` returns a namespace with no type information, so we need
     # to `cast` if we want the Language Server to know what it's looking at.
     materials = cast(pathlib.Path, args.input).read_text().splitlines()
-
-    # Move longer identifiers to the front so, for example, `STONE_BRICK`
-    # matches before `STONE`, and `CHEST_BOAT` matches before `BOAT`.
-    FAMILIES.sort(key=lambda f: len(f.identifier), reverse=True)
-    VARIANTS.sort(key=lambda v: len(v.identifier), reverse=True)
-
-    sort_keys: dict[str, SortKey] = {}
-
-    for material in materials:
-        for variant in VARIANTS:
-            if (family := get_family(material, variant)) is not None:
-                sort_keys[material] = SortKey(
-                    family.category.value,
-                    family.order,
-                    variant.order,
-                )
-                break
-            else:
-                pass
-        else:
-            raise RuntimeError(f"family not found for {material!r}")
+    sort_keys = get_sort_keys(materials)
+    lines = [
+        textwrap.indent(
+            SORTKEY_JAVA_LINE.format(repr(material), sort_key), prefix=" " * 8
+        )
+        for material, sort_key in sort_keys.items()
+    ]
+    contents = "\n".join(lines).strip()
+    file = SORTKEY_JAVA.format(contents)
+    print(file)
 
 
 def get_family(material: str, variant: Variant) -> Family | None:
@@ -85,6 +109,26 @@ def build_cli() -> argparse.ArgumentParser:
     )
 
     return parser
+
+
+def get_sort_keys(materials: list[str]) -> dict[str, SortKey]:
+    sort_keys = {}
+
+    for material in materials:
+        for variant in VARIANTS:
+            if (family := get_family(material, variant)) is not None:
+                sort_keys[material] = SortKey(
+                    family.category.value,
+                    family.order,
+                    variant.order,
+                )
+                break
+            else:
+                pass
+        else:
+            raise RuntimeError(f"family not found for {material!r}")
+
+    return sort_keys
 
 
 if __name__ == "__main__":
